70a4b5697946f289dd05b91fee6caf3a64f5bea3
Todd Stuke <tddstuke@gmail.com>
2022-07-31 08:18:17 -0500

    set up react client front end and synced front/back development servers

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 // import apolloServer
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
 // import our tyedefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
@@ -24,6 +25,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   // integrage our apollo server with the exporess applicatoin as middlware
   server.applyMiddleware({ app });
+
+  // serve up static asssets
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+  }
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  });
 
   db.once("open", () => {
     app.listen(PORT, () => {
